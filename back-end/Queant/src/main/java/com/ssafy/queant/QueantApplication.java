eca49e4e0a348e7bbb35145040154541b9c3ac79
Inyoung.R <inyoung.r@gmail.com>
2022-07-11 11:17:11 +0900

    backend initial commit

---
+++ back-end/Queant/src/main/java/com/ssafy/queant/QueantApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.queant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueantApplication.class, args);
+    }
+
+}
---
+++ back-end/Queant/src/test/java/com/ssafy/queant/QueantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.queant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
