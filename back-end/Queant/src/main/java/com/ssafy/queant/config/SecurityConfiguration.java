1cd6c5844014e57aa78a1dedd85b8c689d068f13
Inyoung.R <inyoung.r@gmail.com>
2022-07-13 17:07:06 +0900

    back_inprogress : MemberService

--- back-end/Queant/src/main/java/com/ssafy/queant/config/SecurityConfiguration.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/config/SecurityConfiguration.java
@@ -39,17 +39,20 @@ public class SecurityConfiguration {
         http
                 .httpBasic().disable() // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
                 .cors().configurationSource(corsConfigurationSource())
+
                 .and()
                 .csrf().disable() // rest api이므로 csrf 보안이 필요없으므로 disable처리.
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증하므로 세션은 필요없으므로 생성안함.
+
                 .and()
                 .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
                 .antMatchers("/*/signin").permitAll() // 가입 및 인증 주소는 누구나 접근가능
-                .antMatchers("/v2/api-docs", "/swagger-resources/**",
-                        "/swagger-ui/**", "/webjars/**", "/swagger/**").permitAll()
+                .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**", "/swagger/**").permitAll()
                 .anyRequest().hasAnyRole("USER","ADMIN", "SUPER")// 그외 나머지 요청은 모두 인증된 회원만 접근 가능
+
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
--- back-end/Queant/src/main/java/com/ssafy/queant/model/service/MemberService.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/service/MemberService.java
@@ -1,4 +1,5 @@
 package com.ssafy.queant.model.service;
 
 public interface MemberService {
+
 }
