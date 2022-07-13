06d0073518f10db769ca6a9e5d2d843488e19d84
sally2596@gmial.com <sally2596@gmail.com>
2022-07-14 01:26:47 +0900

    back_inprogress : add Google OAuth model

---
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/oauth/GoogleOAuthResponse.java
@@ -0,0 +1,16 @@
+package com.ssafy.queant.model.oauth;
+
+import lombok.Data;
+
+@Data
+public class GoogleOAuthResponse {
+
+    private String accessToken;
+    private String expiresIn;
+    private String refreshToken;
+    private String scope;
+    private String tokenType;
+    private String idToken;
+
+
+};
