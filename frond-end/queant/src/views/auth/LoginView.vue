67e66096f0a1e3086676e9642496ad98bc5be48d
이재익 <csljoke@naver.com>
2022-07-18 17:56:06 +0900

    front_feat : 카운트다운 기능임시

--- frond-end/queant/src/views/auth/LoginView.vue
+++ frond-end/queant/src/views/auth/LoginView.vue
@@ -3,19 +3,33 @@
     <h1>QueÆnt</h1>
     <!-- Login Form -->
     <form action="">
+
       <div class="int-area">
-        <input type="text" name="id" id="id"
+        <input 
+        v-model="email"
+        v-bind:class="{error : error.email, complete:!error.email&&email.length!==0}"
+        type="text"
+        id="email"
         autocomplete="off" required>
-        <label for="id">EMAIL</label>
+        <label for="email">EMAIL</label>
+        <div class="error-text" v-if="error.email">{{error.email}}</div>
       </div>
+
       <div class="int-area">
-        <input type="password" name="pw" id="pw"
+        <input
+        v-model="password"
+        v-bind:class="{error : error.password, complete:!error.password&&password.length!==0}"
+        type="password"
+        id="pw"
         autocomplete="off" required>
         <label for="pw">PASSWORD</label>
+        <div class="error-text" v-if="error.password">{{error.password}}</div>
       </div>
+
       <div class="btn-area">
         <button type="submit">LOGIN</button>
       </div>
+
     </form>
     <div class="caption">
       <a href="">비밀번호를 잊으셨나요?</a>
@@ -41,20 +55,35 @@
 <script>
 // @ is an alias to /src
 import { mapActions, mapState } from 'vuex'
+import * as EmailValidator from "email-validator";
+import PV from "password-validator"
 
 export default {
   name: 'LoginView',
   components: {},
+  created() {
+    this.component = this;
+
+    this.passwordSchema
+      .is()
+      .min(8)
+      .is()
+      .max(100)
+      .has()
+      .digits()
+      .has()
+      .letters();
+  },
+  watch: {
+    password: function(v) {
+      this.checkForm();
+    },
+    email: function(v) {
+      this.checkForm();
+    }
+  },
   computed: {
     ...mapState(['token'])
-  },
-  data() {
-    return {
-      credentials: {
-        username: '',
-        password: '',
-      }
-    }
   },
   methods: {
     ...mapActions(['login', 'logout', 'kakaoLogin']),
@@ -65,8 +94,32 @@ export default {
       naver_id_login.setState(state);
       // naver_id_login.setPopup(); // popup 설정을 위한 코드
       naver_id_login.init_naver_id_login();
+    },
+    checkForm() {
+      
+      if (this.email.length > 4 && !EmailValidator.validate(this.email))
+        this.error.email = "올바른 이메일 형식이 아닙니다.";
+      else this.error.email = false;
+      if (this.password.length > 4 && !this.passwordSchema.validate(this.password))
+        this.error.password = "비밀번호는 영문,숫자 포함 8 자리이상이어야 합니다.";
+      else this.error.password = false;
+    },
+  },
+  data() {
+    return {
+      email: "",
+      password : "",
+      passwordSchema: new PV(),
+      error: {
+        email : false,
+        password : false
+      },
+      credentials: {
+        username: '',
+        password: '',
+      }
     }
-  }
+  },
 }
 </script>
 <style>
