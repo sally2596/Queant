74efaecf1a0b28cc833192a8afa2e56962d155ee
Inyoung.R <inyoung.r@gmail.com>
2022-07-19 13:10:13 +0900

    back_refactor : Member entity 변경

--- back-end/Queant/src/main/java/com/ssafy/queant/model/entity/Member.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/entity/Member.java
@@ -2,14 +2,14 @@ package com.ssafy.queant.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Entity
@@ -21,12 +21,23 @@ import java.util.stream.Collectors;
 @Table
 public class Member implements UserDetails {
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private UUID member_id;
+    @Column(nullable = true, unique=true)
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(nullable = false)
     private String password;
     @Column(nullable = false, unique = true)
     private String name;
+    @Column(nullable = true)
+    private Gender gender;
+    @Column(nullable = true)
+    private Date birthdate;
+    @Column(nullable = false)
+    @Builder.Default
+    private int portfolio_cnt = -1;
     @Column(nullable = false)
     @Builder.Default
     private Social social = Social.valueOf("None");
