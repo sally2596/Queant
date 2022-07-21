a7300b48b81e7c32df92bca0cef52f6029d13acd
sally <sally2596@gmail.com>
2022-07-21 14:22:43 +0900

    back_feat: add objectMapper (Bean)

--- back-end/Queant/src/main/java/com/ssafy/queant/config/ApplicationConfig.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/config/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package com.ssafy.queant.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,5 +12,13 @@ public class ApplicationConfig {
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // NULL이 아닌 값만 응답받기(NULL인 경우는 생략)
+        return objectMapper;
     }
 }
