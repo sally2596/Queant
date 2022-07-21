1502e29383dee1daac7dff4282dd2f3e60dbc3cf
Inyoung.R <inyoung.r@gmail.com>
2022-07-21 13:34:57 +0900

    back_feat : refreshtoken, passwordcheck added

--- back-end/Queant/src/main/java/com/ssafy/queant/controller/MemberController.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/controller/MemberController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -24,6 +25,8 @@ public class MemberController {
     private final MemberService memberService;
     private final EmailService emailService;
     private final JwtTokenProvider jwtTokenProvider;
+
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/emailcheck")
     //200은 성공 409중복되었다.
@@ -85,18 +88,37 @@ public class MemberController {
     @DeleteMapping("/info")
     public ResponseEntity<?> DeleteInfo(@RequestBody MemberRequestDto requestDto){
         boolean result = memberService.disableMember(requestDto.getEmail());
-        if(result) new ResponseEntity<>(HttpStatus.OK);
+        if(result) return new ResponseEntity<>(HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
     @PostMapping("/passwordcheck")
-    public ResponseEntity<?> PasswordCheck(){
-        return null;
+    public ResponseEntity<?> PasswordCheck(@RequestBody MemberRequestDto requestDto){
+        MemberDto member = memberService.findMember(requestDto.getEmail());
+        if(member == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if(!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/refreshtoken")
-    public ResponseEntity<?> RefreshToken(){
-        return null;
+    public ResponseEntity<?> RefreshToken(@RequestBody MemberRequestDto requestDto){
+        MemberDto memberDto = memberService.findMember(requestDto.getEmail());
+        if(memberDto == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        //유효한 refreshToken
+        if(jwtTokenProvider.validateToken(requestDto.getRefreshtoken())){
+            //refreshToken 일치 -> accessToken 새로 발급
+            if(memberDto.getRefreshToken().equals(requestDto.getRefreshtoken())){
+                String AccessToken = jwtTokenProvider.createToken(memberDto.getEmail());
+                return new ResponseEntity<String>(AccessToken,HttpStatus.OK);
+            } else { //refreshToken 불일치
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+        } else { //refreshToken 만료 시 로그인 진행
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 
 }
