37c615fee92b28ed83366261e3d319cfe564abc0
jelee6613 <jelee6613@gmail.com>
2022-07-21 13:12:35 +0900

    front_feat : add register form verify

---
+++ front-end/queant/src/components/RegisterCompleted.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>회원가입에 성공하셨습니다.</h1>
+    <h1>다시 로그인 해주세용~~</h1>
+    <router-link :to="{ name: 'login'}">로그인 페이지로~</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterCompleted'
+}
+</script>
+
+<style>
+
+</style>
