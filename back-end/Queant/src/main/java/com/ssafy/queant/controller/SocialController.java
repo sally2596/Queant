f0310b2a964c70091fea669156e0636ad9c090a8
sally2596@gmial.com <sally2596@gmail.com>
2022-07-25 01:16:47 +0900

    back_feat: add naver member parser (to memberdto)

--- back-end/Queant/src/main/java/com/ssafy/queant/controller/SocialController.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/controller/SocialController.java
@@ -1,8 +1,10 @@
 package com.ssafy.queant.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.queant.model.oauth.*;
+import com.ssafy.queant.model.dto.MemberDto;
+import com.ssafy.queant.model.oauth.OAuthResponseDto;
 import com.ssafy.queant.model.service.OAuth.GoogleService;
 import com.ssafy.queant.model.service.OAuth.KakaoService;
 import com.ssafy.queant.model.service.OAuth.NaverService;
@@ -11,8 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -21,12 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLDecoder;
-import java.nio.charset.Charset;
 
 @Controller
 @CrossOrigin("*")
@@ -47,7 +41,7 @@ public class SocialController {
     private ObjectMapper objectMapper;
 
     @Autowired
-    SocialController(GoogleService googleService, KakaoService kakaoService,NaverService naverService) {
+    SocialController(GoogleService googleService, KakaoService kakaoService, NaverService naverService) {
         this.googleService = googleService;
         this.kakaoService = kakaoService;
         this.naverService = naverService;
@@ -55,7 +49,8 @@ public class SocialController {
 
     @GetMapping(value = "/naver")
     public ResponseEntity<Object> getNaverInitUrl() {
-        return ResponseEntity.ok().body(naverService.naverInitUrl());}
+        return ResponseEntity.ok().body(naverService.naverInitUrl());
+    }
 
     @GetMapping(value = "/kakao")
     public ResponseEntity<Object> getKakaoInitUrl() {
@@ -67,127 +62,64 @@ public class SocialController {
         return ResponseEntity.ok().body(googleService.googleInitUrl());
     }
 
-//    @GetMapping(value = "/google")
-//    public ResponseEntity<Object> getGoogleInitUrl() {
-//        return getInitUrl(googleService.googleInitUrl());
-//    }
-
     @GetMapping(value = "/naver/login")
     public ResponseEntity<String> getNaverUser(
             @RequestParam(value = "code") String authCode,
             @RequestParam(value = "state") String state
-    ) {
-        LOGGER.info("state: "+state);
-        // HTTP 통신을 위해 RestTemplate 활용
-        LinkedMultiValueMap<String, String> oAuthRequest= naverService.getNaverOAuthRequest(authCode,state);
+    ) throws JsonProcessingException {
+
+        LinkedMultiValueMap<String, String> oAuthRequest = naverService.getNaverOAuthRequest(authCode, state);
         String accessTokenUrl = naverService.getNaverTokenUrl();
         String userUrl = naverService.getNaverUserUrl();
 
-        try {
-            /***
-             * Authorization Code를 넣어 토큰을 가져오는 작업
-             *
-             * OAuthRequest: 소셜의 OAuth Request 파라미터들을 담음
-             * accessTokenUrl: 각 소셜의 accessToken을 받아올 Url (Authorization Code가 이미 들어가 있음)
-             */
-            ResponseEntity<String> responseJson = getResponseEntity(oAuthRequest,accessTokenUrl);
+        String token = getToken(oAuthRequest,accessTokenUrl);
 
-            /***
-             * 응답받은 Json ->token 추출
-             */
-            OAuthResponseDto responseDto = objectMapper.readValue(responseJson.getBody(),new TypeReference<OAuthResponseDto>() {});
-            String token = responseDto.getAccessToken();
+        String resultData = getUserDatabyAccessToken(userUrl, token);
 
+        MemberDto member = naverService.jsonToMemberDto(resultData);
 
-            /***
-             * token 넣어서 data 가져올 url 생성 -> 사용자 data 받아오기
-             * google, kakao: openId를 통한 데이터 받아오기 가능
-             */
-            String resultData = getUserDatabyAccessTokenTEST(userUrl,token);
-            LOGGER.info("resultData:\n"+uniToKor(resultData));
+        // 멤버가 없으면 저장 + 로그인
 
-            return ResponseEntity.ok().body(resultData);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.badRequest().body(null);
+        // 멤버가 있으면 로그인 처리
+
+        return ResponseEntity.ok().body(resultData);
+
     }
 
     @GetMapping(value = "/kakao/login")
     public ResponseEntity<String> getKakaoUser(
             @RequestParam(value = "code") String authCode
-    ) {
+    ) throws JsonProcessingException {
         // HTTP 통신을 위해 RestTemplate 활용
-        LinkedMultiValueMap<String, String> oAuthRequest= kakaoService.getKakaoOAuthRequest(authCode);
+        LinkedMultiValueMap<String, String> oAuthRequest = kakaoService.getKakaoOAuthRequest(authCode);
         String accessTokenUrl = kakaoService.getKakaoTokenUrl();
         String userUrl = kakaoService.getKakaoUserUrl();
 
-        try {
-            /***
-             * Authorization Code를 넣어 토큰을 가져오는 작업
-             *
-             * OAuthRequest: 소셜의 OAuth Request 파라미터들을 담음
-             * accessTokenUrl: 각 소셜의 accessToken을 받아올 Url (Authorization Code가 이미 들어가 있음)
-             */
-            ResponseEntity<String> responseJson = getResponseEntity(oAuthRequest,accessTokenUrl);
+        String token = getToken(oAuthRequest,accessTokenUrl);
 
-            /***
-             * 응답받은 Json ->token 추출
-             */
-            OAuthResponseDto responseDto = objectMapper.readValue(responseJson.getBody(),new TypeReference<OAuthResponseDto>() {});
-            String token = responseDto.getAccessToken();
+        String resultData = getUserDatabyAccessToken(userUrl, token);
 
-            /***
-             * token 넣어서 data 가져올 url 생성 -> 사용자 data 받아오기
-             * google, kakao: openId를 통한 데이터 받아오기 가능
-             */
-           String resultData = getUserDatabyAccessToken(userUrl,token);
-           LOGGER.info("resultData:\n"+resultData);
-           return ResponseEntity.ok().body(resultData);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.badRequest().body(null);
+
+        return ResponseEntity.ok().body(resultData);
+//        return ResponseEntity.badRequest().body(null);
     }
 
     @GetMapping(value = "/google/login")
     public ResponseEntity<String> getGoogleUser(
             @RequestParam(value = "code") String authCode
-    ) {
+    ) throws JsonProcessingException {
         // HTTP 통신을 위해 RestTemplate 활용
-        LinkedMultiValueMap<String, String> oAuthRequest= googleService.getGoogleOAuthRequest(authCode);
+        LinkedMultiValueMap<String, String> oAuthRequest = googleService.getGoogleOAuthRequest(authCode);
         String accessTokenUrl = googleService.getGoogleTokenUrl();
         String userUrl = googleService.getGoogleUserUrl();
 
-        try {
-            /***
-             * Authorization Code를 넣어 토큰을 가져오는 작업
-             *
-             * OAuthRequest: 소셜의 OAuth Request 파라미터들을 담음
-             * accessTokenUrl: 각 소셜의 accessToken을 받아올 Url (Authorization Code가 이미 들어가 있음)
-             */
-            ResponseEntity<String> responseJson = getResponseEntity(oAuthRequest,accessTokenUrl);
+        String token = getToken(oAuthRequest,accessTokenUrl);
 
-            /***
-             * 응답받은 Json ->token 추출
-             */
-            OAuthResponseDto responseDto = objectMapper.readValue(responseJson.getBody(),new TypeReference<OAuthResponseDto>() {});
-            String token = responseDto.getAccessToken();
+        String resultData = getUserDatabyAccessToken(userUrl, token);
 
-            /***
-             * token 넣어서 data 가져올 url 생성 -> 사용자 data 받아오기
-             * google, kakao: openId를 통한 데이터 받아오기 가능
-             */
-            String userData = getUserDatabyAccessToken(userUrl,token);
-            LOGGER.info("userData:\n"+userData);
-            return ResponseEntity.ok().body(userData);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.badRequest().body(null);
+
+        return ResponseEntity.ok().body(resultData);
+//        return ResponseEntity.badRequest().body(null);
     }
 
     private String getUserDatabyAccessToken(String url, String accessToken) {
@@ -196,52 +128,19 @@ public class SocialController {
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         headers.set("Authorization", "Bearer " + accessToken);
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(null, headers);
-        return restTemplate.exchange(url,HttpMethod.GET,request,String.class).getBody();
+        return restTemplate.exchange(url, HttpMethod.GET, request, String.class).getBody();
     }
 
-    private String getUserDatabyAccessTokenTEST(String url, String accessToken) {
-        // header 설정: AccessToken을 담음
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", "Bearer " + accessToken);
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(null, headers);
-        ResponseEntity<String> tt= restTemplate.exchange(url,HttpMethod.GET,request,String.class);
-        LOGGER.info("tt:\n"+tt);
-        return tt.getBody();
-    }
-
-    private ResponseEntity<String> getResponseEntity(LinkedMultiValueMap<String, String> params,String accessTokenUrl) {
+    private String getToken(LinkedMultiValueMap<String, String> params, String accessTokenUrl) throws JsonProcessingException {
+        // 토큰 요청
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-        return restTemplate.postForEntity(accessTokenUrl, request, String.class);
-    }
+        ResponseEntity<String> responseJson= restTemplate.postForEntity(accessTokenUrl, request, String.class);
 
-    public ResponseEntity<Object> getInitUrl(String authUrl){
-        try {
-            URI redirectUri = new URI(authUrl);
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setLocation(redirectUri);
-            httpHeaders.setAccessControlAllowOrigin("http://localhost:8080");
-            return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.badRequest().build();
-    }
+        OAuthResponseDto responseDto = objectMapper.readValue(responseJson.getBody(), new TypeReference<OAuthResponseDto>() {});
+        String token = responseDto.getAccessToken();
 
-    public String uniToKor(String uni){
-        StringBuffer result = new StringBuffer();
-
-        for(int i=0; i<uni.length(); i++){
-            if(uni.charAt(i) == '\\' &&  uni.charAt(i+1) == 'u'){
-                Character c = (char)Integer.parseInt(uni.substring(i+2, i+6), 16);
-                result.append(c);
-                i+=5;
-            }else{
-                result.append(uni.charAt(i));
-            }
-        }
-        return result.toString();
+        return token;
     }
 }
--- back-end/Queant/src/main/java/com/ssafy/queant/model/service/OAuth/NaverService.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/service/OAuth/NaverService.java
@@ -1,16 +1,17 @@
 package com.ssafy.queant.model.service.OAuth;
 
-import com.ssafy.queant.controller.SocialController;
+import com.ssafy.queant.model.dto.MemberDto;
+import com.ssafy.queant.model.entity.Gender;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
-import java.net.URLEncoder;
 import java.security.SecureRandom;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -102,5 +103,44 @@ public class NaverService {
         params.add("state", state);
         params.add("code", authCode);
         return params;
+    }
+
+    public String uniToKor(String uni){
+        StringBuffer result = new StringBuffer();
+
+        for(int i=0; i<uni.length(); i++){
+            if(uni.charAt(i) == '\\' &&  uni.charAt(i+1) == 'u'){
+                Character c = (char)Integer.parseInt(uni.substring(i+2, i+6), 16);
+                result.append(c);
+                i+=5;
+            }else{
+                result.append(uni.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+    public MemberDto jsonToMemberDto(String resultData) {
+        JSONObject jObject = new JSONObject(resultData) ;
+        JSONObject user = new JSONObject(jObject.get("response").toString());
+
+        int year = Integer.parseInt(user.get("birthyear").toString());
+        int month = Integer.parseInt(user.get("birthday").toString().split("-")[0]);
+        int day = Integer.parseInt(user.get("birthday").toString().split("-")[1]);
+
+        String name = uniToKor(user.get("name").toString());
+        String email = user.get("email").toString();
+        Gender gender = user.get("gender").toString().equals("F")? Gender.Female:Gender.Male;
+        Date birthdate =new Date(year-1900,month-1,day);
+
+
+        MemberDto member = MemberDto.builder()
+                .name(name)
+                .email(email)
+                .gender(gender)
+                .birthdate( birthdate)
+                .build();
+        LOGGER.info("[Naver OAuth member] "+member);
+        return member;
     }
 }
