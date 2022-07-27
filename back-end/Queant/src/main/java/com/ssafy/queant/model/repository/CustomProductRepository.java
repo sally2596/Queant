add9f7af5c02231e1c5dbfe73979e42b1cf7b66e
sally2596@gmail.com <sally2596@gmail.com>
2022-07-27 15:26:40 +0900

    back_feat: find CustomProduct by MemberEmail

--- back-end/Queant/src/main/java/com/ssafy/queant/model/repository/CustomProductRepository.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/repository/CustomProductRepository.java
@@ -2,10 +2,13 @@ package com.ssafy.queant.model.repository;
 
 import com.ssafy.queant.model.entity.product.CustomProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomProductRepository extends JpaRepository<CustomProduct, String> {
-//
-//    @Query("select p from CustomProduct p,Member m join p.member_id where m.email =:email")
-//    List<CustomProduct> findByEmail(@Param("email") String email);
 
+    @Query("select c from CustomProduct c left join Member m on m.member_id=c.member_id where m.email=:email")
+    List<CustomProduct> findByMemberEmail(@Param("email") String email);
 }
