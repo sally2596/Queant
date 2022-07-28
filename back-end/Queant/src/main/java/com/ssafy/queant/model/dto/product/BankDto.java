b8f888b2cd2e73d3a931bb9107c4a2e907f6dd38
sally <sally2596@gmail.com>
2022-07-28 17:55:15 +0900

    back_dto: add bank & product Dto

---
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/dto/product/BankDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.queant.model.dto.product;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class BankDto {
+    private int bankId;
+    private String scodeId;
+    private String bankName;
+    private String homepage;
+    private String tel;
+    private String picture;
+}
