25b49db9aca805cc2f85fb4db4be07a5697758d3
sally <sally2596@gmail.com>
2022-07-28 17:57:54 +0900

    back_controller: inprogress bankController

---
+++ back-end/Queant/src/main/java/com/ssafy/queant/controller/BankController.java
@@ -0,0 +1,53 @@
+package com.ssafy.queant.controller;
+
+import com.ssafy.queant.model.dto.product.BankDto;
+import com.ssafy.queant.model.dto.product.BankResponseDto;
+import com.ssafy.queant.model.dto.product.ProductDto;
+import com.ssafy.queant.model.service.BankService;
+import com.ssafy.queant.model.service.ProductService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@CrossOrigin("*")
+@RequestMapping("/bank")
+public class BankController {
+    private final BankService bankService;
+    private final ProductService productService;
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message="모든 은행정보를 가져왔습니다."),
+    })
+    @Operation(summary = "모든 은행 정보 받아옴")
+    @GetMapping("/")
+    public ResponseEntity<?> getAllBank() throws Exception {
+        return new ResponseEntity<List<BankDto>>(bankService.findAll(),HttpStatus.OK);
+    }
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message="모든 은행정보를 가져왔습니다."),
+    })
+    @Operation(summary = "모든 은행 정보 받아옴")
+    @GetMapping("/info")
+    public ResponseEntity<?> getBankByBankId(@RequestParam int bankId) throws Exception {
+        BankDto bankDto =bankService.findByBankId(bankId);
+        List<ProductDto> productDtoList = productService.findByBankId(bankId);
+
+        BankResponseDto responseDto = BankResponseDto.builder()
+                .bank(bankDto)
+                .productList(productDtoList)
+                .build();
+
+        return new ResponseEntity<BankResponseDto>(responseDto,HttpStatus.OK);
+    }
+}
