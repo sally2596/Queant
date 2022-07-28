705c84616e9236f12bd4427fd322abebc8d63179
Inyoung.R <inyoung.r@gmail.com>
2022-07-28 16:57:17 +0900

    back_fix : gitignore build changed

---
+++ back-end/Queant/build/generated/sources/annotationProcessor/java/main/com/ssafy/queant/model/entity/QMember.java
@@ -0,0 +1,58 @@
+package com.ssafy.queant.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -1347005153L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final DatePath<java.sql.Date> birthdate = createDate("birthdate", java.sql.Date.class);
+
+    public final StringPath email = createString("email");
+
+    public final BooleanPath enabled = createBoolean("enabled");
+
+    public final EnumPath<Gender> gender = createEnum("gender", Gender.class);
+
+    public final ComparablePath<java.util.UUID> member_id = createComparable("member_id", java.util.UUID.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    public final NumberPath<Integer> portfolio_cnt = createNumber("portfolio_cnt", Integer.class);
+
+    public final StringPath refreshToken = createString("refreshToken");
+
+    public final SetPath<MemberRole, EnumPath<MemberRole>> roleSet = this.<MemberRole, EnumPath<MemberRole>>createSet("roleSet", MemberRole.class, EnumPath.class, PathInits.DIRECT2);
+
+    public final EnumPath<Social> social = createEnum("social", Social.class);
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
