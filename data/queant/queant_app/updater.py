5400b9d1e7c7430b191c484fc9af28fb4117e60d
DongHyeon <72799490+Kameokong@users.noreply.github.com>
2022-08-01 16:08:06 +0900

    data_parsing: update code(save datas in db with apscheduler)

--- data/queant/queant_app/updater.py
+++ data/queant/queant_app/updater.py
@@ -1,9 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from .save_db import save_prdt
+from .save_to_db import save_db
     
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(save_prdt, 'interval', seconds=5)
+    scheduler.add_job(save_db, trigger='cron', day_of_week='mon', hour=15, minute=52)
     scheduler.start()
