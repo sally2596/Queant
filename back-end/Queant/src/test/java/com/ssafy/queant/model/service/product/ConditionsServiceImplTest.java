7089f8aee78a79880ebdc5110e40110dea066f64
sally2596@gmail.com <sally2596@gmail.com>
2022-08-03 02:16:00 +0900

    back_service: ConditionsService (+test)

---
+++ back-end/Queant/src/test/java/com/ssafy/queant/model/service/product/ConditionsServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.ssafy.queant.model.service.product;
+
+import com.ssafy.queant.model.dto.product.ConditionsDto;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class ConditionsServiceImplTest {
+
+    private final Logger log = LoggerFactory.getLogger(ConditionsServiceImplTest.class);
+
+    @Autowired
+    private ConditionsService conditionsService;
+
+    @Test
+    void findByProductId() {
+        String productId = "WR0001B";
+        List<ConditionsDto> result = conditionsService.findByProductId(productId);
+
+        for (ConditionsDto dto:result
+        ) {
+            log.info(dto.toString());
+        }
+    }
+}
