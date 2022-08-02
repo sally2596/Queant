ca39624037f02b86ada5f4367e6f54e01690ce94
sally2596@gmail.com <sally2596@gmail.com>
2022-08-03 01:37:36 +0900

    back_service: add getSearchKeyword (+test)

---
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/service/SearchService.java
@@ -0,0 +1,7 @@
+package com.ssafy.queant.model.service;
+
+import com.ssafy.queant.model.dto.Search.SearchKeywordDto;
+
+public interface SearchService {
+    SearchKeywordDto getSearchKeyword();
+}
