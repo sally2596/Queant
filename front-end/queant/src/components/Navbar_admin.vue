42b12af981edf7b2f994ecdff6677a9339ce28b2
이재익 <csljoke@gmail.com>
2022-08-02 15:43:08 +0900

    front_feat_refactor_design : 어드민 네브바 잠깐 만듦(메뉴 추가하면 됨), 유저 관리 리스트 CSS 입히기

---
+++ front-end/queant/src/components/Navbar_admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header" id="header">
+    <nav class="navbar navbar-expand-lg bg-none" id="admin_navbar">
+      <router-link to="/">
+        <img class="logo mr-4" src="../assets/image/퀸트_로고.png" alt="">
+      </router-link>
+      <p>ADMIN PAGE</p>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Navbar_admin",
+}
+</script>
+
+<style>
+@import '../assets/css/navbar.css';
+</style>
