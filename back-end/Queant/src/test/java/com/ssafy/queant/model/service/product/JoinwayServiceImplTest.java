4757b789b41dee79b4ac629d68dd9b78434a4907
sally2596@gmail.com <sally2596@gmail.com>
2022-08-02 13:22:51 +0900

    back_test: joinway test (repository & service)

---
+++ back-end/Queant/src/test/java/com/ssafy/queant/model/service/product/JoinwayServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.ssafy.queant.model.service.product;
+
+import com.ssafy.queant.model.dto.product.JoinwayDto;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class JoinwayServiceImplTest {
+    private final Logger log = LoggerFactory.getLogger(JoinwayServiceImplTest.class);
+
+    @Autowired
+    private JoinwayService joinwayService;
+
+    @Test
+    void findByProductId() {
+        String productId = "WR0001A";
+        List<JoinwayDto> result = joinwayService.findByProductId(productId);
+
+        for (JoinwayDto dto:result
+             ) {
+            log.info(dto.toString());
+        }
+    }
+
+}
