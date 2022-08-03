0ef826381588eafafd9fc482f0a8dec82a6500c5
DongHyeon <72799490+Kameokong@users.noreply.github.com>
2022-08-03 10:45:26 +0900

    data_release: update code to save bankinfo in django server

--- data/queant/queant_app/save_to_db.py
+++ data/queant/queant_app/save_to_db.py
@@ -219,6 +219,34 @@ def spcl_parsing(spcl):
         data_list.append(data)
     return data_list   
 
+
+def change_tel(tel):
+    if len(tel) == 8:
+        tel_front = tel[:4]
+        tel_back = tel[4:]
+        tel = tel_front + "-" + tel_back
+    elif len(tel) == 9:
+        tel_front = tel[:2]
+        tel_middle = tel[2:5]
+        tel_back = tel[5:]
+        tel = tel_front + '-' + tel_middle + '-' + tel_back
+    elif len(tel) == 10 and tel[0] == '0' and tel[1] == '2':
+        tel_front = tel[:2]
+        tel_middle = tel[2:6]
+        tel_back = tel[6:]
+        tel = tel_front + '-' + tel_middle + '-' + tel_back
+    elif len(tel) == 10:
+        tel_front = tel[:3]
+        tel_middle = tel[3:6]
+        tel_back = tel[6:]
+        tel = tel_front + '-' + tel_middle + '-' + tel_back
+    elif len(tel) == 11:
+        tel_front = tel[:3]
+        tel_middle = tel[3:7]
+        tel_back = tel[7:]
+        tel = tel_front + '-' + tel_middle + '-' + tel_back
+    return tel
+
 def connect_db():
     conn = pymysql.connect(host="i7a201.p.ssafy.io", port = 3306, user='queant', password='A201Queant', db = 'queant', charset='utf8mb4')
     cur = conn.cursor()
@@ -373,7 +401,41 @@ def save_into_db(cur, conn, data_xml, is_deposit):
                 
     conn.commit()
     
-    
+
+def save_bank_into_db(cur, conn, data_xml, banktype_num):
+    cur.execute("SELECT code_id FROM queant.common_code where code_value = \"은행 분류\"")
+
+    row = cur.fetchone()
+    if row != None:
+        common_code_bank = row[0] #D가 저장됨
+        
+    query_find_bank = """SELECT * FROM queant.specific_code where code_id = (%s)"""
+    cur.execute(query_find_bank,common_code_bank)
+    bank_types = {}
+    while True:
+        row = cur.fetchone()
+        if row == None:
+            break
+        bank_types[row[2]] = row[0]
+        
+    query_bank_search = """select * from queant.bank where bank_id = (%s);""" #중복체크 확인 쿼리문
+    query_bank = """INSERT INTO queant.bank (bank_id, scode_id, bank_name, homepage, tel) values (%s,%s,%s,%s,%s);""" #데이터 insert 쿼리문
+    for bank_tag in data_xml[5]:
+        bank_code = bank_tag[0].find("fin_co_no").text
+        bank_name = bank_tag[0].find("kor_co_nm").text
+        if banktype_num == 0:
+            bank_type = bank_types["은행"]
+        else:
+            bank_type = bank_types["저축은행"]
+        homepage = bank_tag[0].find("homp_url").text
+        tel = change_tel(bank_tag[0].find("cal_tel").text)
+        cur.execute(query_bank_search, bank_code)
+        if cur.fetchone() == None:
+            values = (bank_code, bank_type, bank_name, homepage, tel)
+            cur.execute(query_bank, values) 
+
+    conn.commit()
+
 def save_db():
     #DB연결
     conn, cur = connect_db()
@@ -438,3 +500,21 @@ def save_db():
         
     conn.close()
     
+    
+def save_bank_db():
+    conn, cur = connect_db()
+    bankcode = ["020000","030300"]
+    
+    banktype_num = 0
+    url = "http://finlife.fss.or.kr/finlifeapi/companySearch.xml?auth=47c0e868fdb16333d47d0e385641c3c0&topFinGrpNo=" + bankcode[banktype_num]+ "&pageNo=1"
+    data_str = urllib.request.urlopen(url).read().decode('euc-kr')
+    data_xml = ET.fromstring(data_str)
+    save_bank_into_db(cur, conn, data_xml, banktype_num)
+    
+    banktype_num = 1
+    url = "http://finlife.fss.or.kr/finlifeapi/companySearch.xml?auth=47c0e868fdb16333d47d0e385641c3c0&topFinGrpNo=" + bankcode[banktype_num]+ "&pageNo=1"
+    data_str = urllib.request.urlopen(url).read().decode('euc-kr')
+    data_xml = ET.fromstring(data_str)
+    save_bank_into_db(cur, conn, data_xml, banktype_num)
+    
+    conn.close()
--- data/queant/queant_app/updater.py
+++ data/queant/queant_app/updater.py
@@ -1,9 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from .save_to_db import save_db
-    
+from .save_to_db import save_bank_db
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(save_db, trigger='cron', day_of_week='mon', hour=16, minute=27)
+    scheduler.add_job(save_db, 'interval', seconds = 30)
+    scheduler.add_job(save_bank_db, 'interval', seconds = 30)
+    #scheduler.add_job(save_db, trigger='cron', day_of_week='wed', hour=10, minute=50)
+    #scheduler.add_job(save_bank_db, trigger='cron', day_of_week='mon', hour=10, minute=50)
+    print("start!")
     scheduler.start()
