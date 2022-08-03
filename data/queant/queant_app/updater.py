7d75550d4fdb5ceb3df83c327316a3aae85b4dbe
DongHyeon <72799490+Kameokong@users.noreply.github.com>
2022-08-03 11:23:52 +0900

    data_release: update code to debug scheduling, timezone check

--- data/queant/queant_app/updater.py
+++ data/queant/queant_app/updater.py
@@ -1,18 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import timezone
 
 from .save_to_db import save_db
 from .save_to_db import save_bank_db
 
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
     #scheduler.add_job(save_db, 'interval', seconds = 10)
     #scheduler.add_job(save_bank_db, 'interval', seconds = 10)
     #scheduler.add_job(save_db, trigger='cron', day_of_week='wed', hour=11, minute=15)
-    scheduler.add_job(save_db, trigger='cron', hour=11, minute=18)
+    scheduler.add_job(save_db, trigger='cron', hour=11, minute=26)
     scheduler.start()
     
     
 def start_bank():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(save_bank_db, trigger='cron',hour=11, minute=18)
+    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
+    scheduler.add_job(save_bank_db, trigger='cron',hour=11, minute=27)
     scheduler.start()    
