27ab6f6464b39e39e4269c091a1748ffcf052e4b
DongHyeon <72799490+Kameokong@users.noreply.github.com>
2022-08-03 11:08:41 +0900

    data_release: update code to debug release server

--- data/queant/queant_app/apps.py
+++ data/queant/queant_app/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class QueantAppConfig(AppConfig):
     name = 'queant_app'
     from . import updater
+    print("Sever running!")
     updater.start()
--- data/queant/queant_app/updater.py
+++ data/queant/queant_app/updater.py
@@ -5,9 +5,8 @@ from .save_to_db import save_bank_db
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(save_db, 'interval', seconds = 30)
-    scheduler.add_job(save_bank_db, 'interval', seconds = 30)
-    #scheduler.add_job(save_db, trigger='cron', day_of_week='wed', hour=10, minute=50)
+    #scheduler.add_job(save_db, 'interval', seconds = 10)
+    #scheduler.add_job(save_bank_db, 'interval', seconds = 10)
+    scheduler.add_job(save_db, trigger='cron', day_of_week='wed', hour=11, minute=15)
     #scheduler.add_job(save_bank_db, trigger='cron', day_of_week='mon', hour=10, minute=50)
-    print("start!")
     scheduler.start()
