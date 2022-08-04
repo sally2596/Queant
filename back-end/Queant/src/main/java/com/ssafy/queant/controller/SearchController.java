4b25a730b647a09555e94d57f4a4433daf67775d
sally <sally2596@gmail.com>
2022-08-04 17:42:00 +0900

    back_feat: wireframe of SearchController

--- back-end/Queant/src/main/java/com/ssafy/queant/controller/SearchController.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.ssafy.queant.controller;
 
 import com.ssafy.queant.model.dto.Search.SearchKeywordDto;
+import com.ssafy.queant.model.dto.Search.SearchRequestDto;
 import com.ssafy.queant.model.dto.product.ProductDto;
 import com.ssafy.queant.model.service.SearchService;
 import com.ssafy.queant.model.service.product.ProductService;
@@ -26,19 +27,18 @@ public class SearchController {
     private final ProductService productService;
 
     @ApiResponses({
-            @ApiResponse(code = 200, message="검색 키워드 목록 가져오기 성공"),
+            @ApiResponse(code = 200, message = "검색 키워드 목록 가져오기 성공"),
     })
     @Operation(summary = "검색 키워드 정보", description = "가입방법, 은행타입, 은행, 우대조건 제공")
     @GetMapping("/keyword")
-    //200은 성공 409중복되었다.
     public ResponseEntity<?> getSearchKeyword() throws Exception {
         SearchKeywordDto keywordDto = searchService.getSearchKeyword();
-        return new ResponseEntity<>(keywordDto,HttpStatus.OK);
+        return new ResponseEntity<>(keywordDto, HttpStatus.OK);
     }
 
     @ApiResponses({
-            @ApiResponse(code = 200, message="검색 결과 가져오기 성공"),
-            @ApiResponse(code = 404, message="검색 정보가 존재하지 않습니다."),
+            @ApiResponse(code = 200, message = "검색 결과 가져오기 성공"),
+            @ApiResponse(code = 404, message = "검색 정보가 존재하지 않습니다."),
     })
     @Operation(summary = "텍스트로 상품 찾기", description = "Nav 바에서 검색할 경우 상품 보여주기")
     @GetMapping()
@@ -46,9 +46,44 @@ public class SearchController {
             @RequestParam(value = "keyword") String keyword
     ) {
         List<ProductDto> list = productService.findByNameContaining(keyword);
-        if(list.size()>0)
-            return new ResponseEntity<>(list,HttpStatus.OK);
+        if (list.size() > 0)
+            return new ResponseEntity<>(list, HttpStatus.OK);
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "예금 단품 검색 성공"),
+            @ApiResponse(code = 404, message = "검색 정보가 존재하지 않습니다."),
+    })
+    @Operation(summary = "예금 단품 검색", description = "키워드로 예금 단품 검색")
+    @PostMapping(value = "/deposit")
+    public ResponseEntity<?> getDepositSingle(@RequestBody SearchRequestDto searchRequestDto) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "적금 단품 검색 성공"),
+            @ApiResponse(code = 404, message = "검색 정보가 존재하지 않습니다."),
+    })
+    @Operation(summary = "적금 단품 검색", description = "키워드로 적금 단품 검색")
+    @PostMapping(value = "/deposit")
+    public ResponseEntity<?> getSavingSingle(@RequestBody SearchRequestDto searchRequestDto) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "적금 세트 검색 성공"),
+            @ApiResponse(code = 404, message = "검색 정보가 존재하지 않습니다."),
+    })
+    @Operation(summary = "적금 세트 검색", description = "키워드로 적금 세트 검색")
+    @PostMapping(value = "/deposit")
+    public ResponseEntity<?> getSavingSet(@RequestBody SearchRequestDto searchRequestDto) {
+
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
 }
