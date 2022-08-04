4a326d1a9a3f186921b6e6cdbd019e698799d2bb
sally <sally2596@gmail.com>
2022-08-04 10:52:24 +0900

    back_repository:findByIsEnabledFalse(for admin accept userInput product)

--- back-end/Queant/src/main/java/com/ssafy/queant/model/repository/product/ProductRepository.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/repository/product/ProductRepository.java
@@ -7,20 +7,23 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product,String> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 
     // 단품 검색 (검색어 조합) == 추천
 
     // 상품 검색 (텍스트 검색, 상품명으로 검색)
     List<Product> findByIsEnabledTrueAndNameContaining(@Param("name") String name);
+
     List<Product> findByNameContaining(@Param("name") String name);
 
     // 세트 검색 (검색어 조합) == 추천
 
-    // 서비스에서 공식적으로 제공하는 모든 상품 == isEnabled가 true인 상품
+    // admin이 사용자들이 입력한 상품 확인(isEnabled = false인 상품 )
+    List<Product> findByIsEnabledFalse();
 
     // 은행별로 묶어서 제공
     List<Product> findByBankIdAndIsEnabledTrue(int bankId);
+
     List<Product> findByBankId(int bankId);
 
     // 상품 세부 정보 제공
