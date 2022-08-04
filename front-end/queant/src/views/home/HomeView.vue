e7f2aa66997dbe5f335d43f9036b8a4ad6e574fa
이재익 <csljoke@naver.com>
2022-08-04 11:51:56 +0900

    front_refactor : home 수정

--- front-end/queant/src/views/home/HomeView.vue
+++ front-end/queant/src/views/home/HomeView.vue
@@ -1,25 +1,16 @@
 <template>
-    <Navbar/> 
-    <section id="hero" class="d-flex justify-cntent-center align-items-center">
-      <div id="heroCarousel" class="container carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
-        <div class="carousel-item active">
-          <div class="carousel-container">
-            <h2>Save Your Moeny, Safely. QueÆnt</h2>
-            <router-link :to="{ name: 'portfolio' }" class="btn-get-started">내 포트폴리오 만들기</router-link>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End Hero -->
+    <Navbar/>
+    <HomeComponents/>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
-
+import HomeComponents from '@/components/HomeComponents.vue'
 export default {
   name: 'HomeView',
   components : {
-    Navbar : Navbar
+    Navbar : Navbar,
+    HomeComponents : HomeComponents
   },
   beforeCreate: function() {
     document.body.className = 'home_body'
