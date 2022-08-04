f0d3273bc06bd15b26ac3c2d1f1a1f930b6311a4
이재익 <csljoke@naver.com>
2022-08-04 17:51:16 +0900

    front_refactor : bankinfo 1차 완료

--- front-end/queant/src/views/bankinfo/BankInfoListView.vue
+++ front-end/queant/src/views/bankinfo/BankInfoListView.vue
@@ -5,14 +5,50 @@
   </header>
   
   <!-- 은행 목록 -->
-  <section class="home">
-    <div class="container row home" id="container_banks">
-      <div
-        v-for="bank in banks" :key="bank.bank_id"
-        class="col-6 col-lg-2 p-3 home">
-        <router-link :to="{ name: 'bankInfoDetail' , params: { bankId: bank.bank_id }}">
-          {{ bank.bank_name }}
-        </router-link>
+    <!-- 제 1금융 목록 -->
+  
+  <h1 class="bank-title">은행</h1>
+  <section class="section-floater">
+    <div id="featured_banks">
+      <div class="row">
+        <div class="col-lg-4 col-6 my-2" v-for="bank in banks" :key="bank.bank_id" v-show="bank.scode_id=='C001'">
+          <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{bank.bank_name}}</h5>
+                <a :href="bank.homepage">홈페이지</a>
+                <p>전화번호 : {{bank.tel}}</p>
+                <router-link class="banks btn btn-outline-success btn-sm" 
+                :to="{ name: 'bankInfoDetail' , params: { bankId: bank.bank_id }}"
+                style="height: 35px;">
+                  <p style="font-size:14px;">상품 보러가기</p>
+                </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+    <br><br><br><br><br><br><br>
+    <!-- 제 2금융 목록 -->
+  
+  <h1 class="bank-title">저축은행</h1>
+  <section class="section-floater">
+    <div id="featured_banks">
+      <div class="row">
+        <div class="col-lg-4 col-6 my-2" v-for="bank in banks" :key="bank.bank_id" v-show="bank.scode_id=='C002'">
+          <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{bank.bank_name}}</h5>
+                <a :href="bank.homepage">홈페이지</a>
+                <p>전화번호 : {{bank.tel}}</p>
+                <router-link class="banks btn btn-outline-success btn-sm"
+                :to="{ name: 'bankInfoDetail' , params: { bankId: bank.bank_id }}"
+                style="height: 35px; text-align: center;">
+                  <p style="font-size: 14px;">상품 보러가기</p>
+                </router-link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -26,13 +62,13 @@ export default {
   name: 'BankInfoListView',
   components : { Navbar },
   computed: {
-    ...mapGetters(['banks'])
+    ...mapGetters(['banks']),
   },
   methods: {
-    ...mapActions(['fetchBanks'])
+    ...mapActions(['fetchBanks']),
   },
   beforeCreate: function() {
-      document.body.className = 'home_body'
+      document.body.className = 'bank_body'
   },
   created() {
     this.fetchBanks()
