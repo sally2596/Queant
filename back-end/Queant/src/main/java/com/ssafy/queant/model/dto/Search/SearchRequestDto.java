2ef3a1db4135237fa27fb3a1d520ebd73697d1e2
sally <sally2596@gmail.com>
2022-08-04 17:41:38 +0900

    back_entity: SearchRequestDto

---
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/dto/Search/SearchRequestDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.queant.model.dto.Search;
+
+import java.util.List;
+
+public class SearchRequestDto {
+    Long amount;
+    int period;
+    boolean isSimpleInterest; // true: 단리, false: 복리
+    List<BankKeywordDto> bank;
+    List<SpecificCodeDto> joinway; // 가입방법
+    List<SpecificCodeDto> conditions; // 우대사항
+    List<SpecificCodeDto> bankType; // 은행 타입
+    List<SpecificCodeDto> traitSet; // 특징
+}
