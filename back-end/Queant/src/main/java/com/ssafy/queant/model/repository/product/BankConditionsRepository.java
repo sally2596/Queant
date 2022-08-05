9ae08b72a279d06615b4b1891a2c83a3f9e8d4f2
sally2596@gmail.com <sally2596@gmail.com>
2022-08-05 10:29:56 +0900

    back_chore: remove BankConditions

--- back-end/Queant/src/main/java/com/ssafy/queant/model/repository/product/BankConditionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.queant.model.repository.product;
-
-import com.ssafy.queant.model.entity.product.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankConditionsRepository extends JpaRepository<Bank, Integer> {
-    
-}
