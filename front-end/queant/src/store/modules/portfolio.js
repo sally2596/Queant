e15bd769f20553308ef0e61c02bd7574e4a1acc7
jelee6613 <jelee6613@gmail.com>
2022-08-05 17:48:54 +0900

    front_feat : add portfolio api

--- front-end/queant/src/store/modules/portfolio.js
+++ front-end/queant/src/store/modules/portfolio.js
@@ -3,11 +3,18 @@ import router from '@/router'
 import axios from 'axios'
 
 export default {
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    portfolio: [],
+    customPortfolio: []
+  },
+  getters: {
+    portfolio: state => state.portfolio
+  },
+  mutations: {
+    SET_PORTFOLIO: (state, portfolio) => state.portfolio = portfolio
+  },
   actions: {
-    fetchPortfolio({ commit }) {
+    fetchPortfolio({ commit, getters }) {
       axios({
         url: spring.portfolio.posession(),
         method: 'post',
@@ -17,6 +24,7 @@ export default {
       })
       .then(res => {
         console.log(res)
+        commit('SET_PORTFOLIO', res.data.portfolio_list)
       })
       .catch(err => {
         console.log(err)
