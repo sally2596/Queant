9fd6a09b656620addaecbf5ff86eb59933a9b2f1
Inyoung.R <inyoung.r@gmail.com>
2022-08-10 14:00:09 +0900

    back_release:swagger fix

--- back-end/Queant/src/main/java/com/ssafy/queant/config/SwaggerConfig.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/config/SwaggerConfig.java
@@ -9,9 +9,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 //http://localhost/swagger-ui.html
 //http://localhost/swagger-ui/index.html
@@ -21,13 +24,14 @@ public class SwaggerConfig{
 
 	@Bean
 	public Docket api() {
+		Server server = new Server("Queant", "https://i7a201.p.ssafy/api", "API TEST", Collections.emptyList(), Collections.emptyList());
 		return new Docket(DocumentationType.OAS_30)
+				.servers(server)
 				.groupName("Queant")
 				.apiInfo(apiInfo())
 				.useDefaultResponseMessages(false)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.ssafy.queant.controller"))
-//				.paths(PathSelectors.ant("/book/**"))
 				.build();
 	}
 
