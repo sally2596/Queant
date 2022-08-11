aa47e4037157bf2bcf9726d324e8ba3b1ab18c5a
DongHyeon <72799490+Kameokong@users.noreply.github.com>
2022-08-11 11:26:27 +0900

    data_release: update code to save post data by django

--- data/queant/queant_app/updater.py
+++ data/queant/queant_app/updater.py
@@ -3,13 +3,13 @@ from pytz import timezone
 
 from .save_to_db import save_db
 from .save_to_db import save_bank_db
+from .save_post_to_db import save_postdata_db
 
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
     #scheduler.add_job(save_db, 'interval', seconds = 10)
     #scheduler.add_job(save_bank_db, 'interval', seconds = 10)
     #scheduler.add_job(save_db, trigger='cron', day_of_week='wed', hour=11, minute=15)
-    #scheduler.add_job(save_db, trigger='cron', hour=17, minute=34)
     scheduler.add_job(save_db, trigger='cron', hour=18, minute=17)
     scheduler.start()
     
@@ -17,4 +17,10 @@ def start():
 def start_bank():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
     scheduler.add_job(save_bank_db, trigger='cron',hour=11, minute=29)
-    scheduler.start()    
+    scheduler.start()
+    
+    
+def start_post():
+    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
+    scheduler.add_job(save_postdata_db, trigger='cron',hour=11, minute=28)
+    scheduler.start()
