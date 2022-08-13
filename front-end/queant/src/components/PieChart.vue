e934141e4a2e1f9da45e2af555f2178c1e350df4
sally2596@gmail.com <sally2596@gmail.com>
2022-08-14 04:07:56 +0900

    front_feat: add PieChart.vue

---
+++ front-end/queant/src/components/PieChart.vue
@@ -0,0 +1,42 @@
+<template>
+  <apexchart
+    type="pie"
+    width="380"
+    :options="chartOptions"
+    :series="series"
+  ></apexchart>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  name: "PieChart",
+  components: { apexchart: VueApexCharts },
+  props: ["series", "chartOptionLabels"],
+  data() {
+    const chartOptions = {
+      chart: {
+        width: 380,
+        type: "pie",
+      },
+      labels: this.chartOptionLabels,
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200,
+            },
+            legend: {
+              position: "left",
+            },
+          },
+        },
+      ],
+    };
+    return { chartOptions };
+  },
+};
+</script>
+
+<style></style>
