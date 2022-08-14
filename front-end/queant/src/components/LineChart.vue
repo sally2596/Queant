e2e734affb3f1d5c09bdb535e2865cfff623f040
sally2596@gmail.com <sally2596@gmail.com>
2022-08-14 17:41:11 +0900

    front_feat: add LineChart & TimeLineChart

---
+++ front-end/queant/src/components/LineChart.vue
@@ -0,0 +1,78 @@
+<template>
+  <apexchart
+    type="line"
+    height="350"
+    :options="chartOptions"
+    :series="series"
+  ></apexchart>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  name: "LineChart",
+  components: { apexchart: VueApexCharts },
+  props: ["series"],
+  data() {
+    const chartOptions = {
+      chart: {
+        type: "area",
+        stacked: false,
+        height: 350,
+        zoom: {
+          type: "x",
+          enabled: true,
+          autoScaleYaxis: true,
+        },
+        toolbar: {
+          autoSelected: "zoom",
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+        size: 0,
+      },
+      title: {
+        text: "Stock Price Movement",
+        align: "left",
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100],
+        },
+      },
+      yaxis: {
+        labels: {
+          formatter: function (val) {
+            return (val / 1000000).toFixed(0);
+          },
+        },
+        title: {
+          text: "Price",
+        },
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      tooltip: {
+        shared: false,
+        y: {
+          formatter: function (val) {
+            return (val / 1000000).toFixed(0);
+          },
+        },
+      },
+    };
+    return { chartOptions };
+  },
+};
+</script>
+
+<style></style>
