3873640e4abc19b571c61b2030fc4bc93ca1cad0
Inyoung.R <inyoung.r@gmail.com>
2022-08-14 22:59:52 +0900

    back_fix: customDto date format

--- back-end/Queant/src/main/java/com/ssafy/queant/model/dto/product/CustomProductDto.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/model/dto/product/CustomProductDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.queant.model.dto.product;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -22,6 +23,8 @@ public class CustomProductDto {
     private boolean isFixedRsrv;
     private String etc; // 기타 사항
     private Long amount; // 예금일때 한번에 넣을 금액
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Date endDate;
 }
