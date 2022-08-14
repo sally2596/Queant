a420f7115f401fd3f6bb657cdd1daa416bb57d2e
sally2596@gmail.com <sally2596@gmail.com>
2022-08-14 22:03:32 +0900

    front_feat: add ColumnChart & LineChart

---
+++ front-end/queant/src/components/ColumnChart.vue
@@ -0,0 +1,55 @@
+<template>
+  <apexchart
+    type="bar"
+    height="500"
+    :options="chartOptions"
+    :series="series"
+  ></apexchart>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  name: "ColumnChart",
+  components: { apexchart: VueApexCharts },
+  props: ["series"],
+  data() {
+    const chartOptions = {
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        toolbar: {
+          show: true,
+        },
+        zoom: {
+          enabled: true,
+        },
+      },
+
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          borderRadius: 5,
+        },
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      theme: {
+        palette: "palette4",
+      },
+      tooltip: {
+        intersect: false,
+        shared: true,
+      },
+      fill: {
+        opacity: 1,
+      },
+    };
+    return { chartOptions };
+  },
+};
+</script>
+
+<style></style>
