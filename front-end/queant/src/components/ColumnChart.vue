692d19b6df2b111aab78e228c797682657acde96
sally2596@gmail.com <sally2596@gmail.com>
2022-08-15 14:17:35 +0900

    front_fix: fix sync Chart

--- front-end/queant/src/components/ColumnChart.vue
+++ front-end/queant/src/components/ColumnChart.vue
@@ -3,51 +3,91 @@
     type="bar"
     height="500"
     :options="chartOptions"
-    :series="series"
+    :series="realSeries"
   ></apexchart>
 </template>
 
 <script>
+import { functionTypeAnnotation } from "@babel/types";
 import VueApexCharts from "vue3-apexcharts";
 export default {
   name: "ColumnChart",
   components: { apexchart: VueApexCharts },
-  props: ["series"],
-  data() {
-    const chartOptions = {
-      chart: {
-        type: "bar",
-        height: 350,
-        stacked: true,
-        toolbar: {
-          show: true,
+  props: ["series", "category"],
+  watch: {
+    series: function () {
+      this.updateSeriesLine();
+    },
+    category: function () {
+      this.updateSeriesLine();
+    },
+  },
+  methods: {
+    updateSeriesLine() {
+      this.realSeries = this.series;
+      this.chartOptions = {
+        chart: {
+          type: "bar",
+          height: 500,
+          stacked: true,
+          toolbar: {
+            show: true,
+          },
+          zoom: {
+            enabled: true,
+          },
         },
-        zoom: {
-          enabled: true,
-        },
-      },
 
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          borderRadius: 5,
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            borderRadius: 3,
+          },
         },
-      },
-      xaxis: {
-        type: "datetime",
-      },
-      theme: {
-        palette: "palette4",
-      },
-      tooltip: {
-        intersect: false,
-        shared: true,
-      },
-      fill: {
-        opacity: 1,
-      },
-    };
-    return { chartOptions };
+        xaxis: {
+          type: "datetime",
+          categories: this.category,
+          labels: {
+            format: "yyyy/MM",
+          },
+        },
+        yaxis: {
+          labels: {
+            formatter: function (val) {
+              return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원";
+            },
+          },
+        },
+        theme: {
+          palette: "palette4",
+        },
+        tooltip: {
+          intersect: false,
+          shared: true,
+          x: {
+            format: "yyyy년 MM월",
+          },
+          y: {
+            formatter: function (val) {
+              return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원";
+            },
+          },
+        },
+        legend: {
+          position: "top",
+          offsetY: 5,
+        },
+        fill: {
+          opacity: 1,
+        },
+      };
+    },
+  },
+  data() {
+    const chartOptions = {};
+    const realCategory = [];
+    const realSeries = [];
+    return { chartOptions, realCategory, realSeries };
   },
 };
 </script>
