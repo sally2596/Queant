9d7175067b8f38a6fb2c1548e835096fc96c3062
Inyoung.R <inyoung.r@gmail.com>
2022-08-15 18:43:28 +0900

    back_inprogress: fixing birthdate format

--- back-end/Queant/src/main/java/com/ssafy/queant/QueantApplication.java
+++ back-end/Queant/src/main/java/com/ssafy/queant/QueantApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class QueantApplication {
@@ -11,6 +14,11 @@ public class QueantApplication {
     //없으면 timeout걸림
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
     public static void main(String[] args) {
