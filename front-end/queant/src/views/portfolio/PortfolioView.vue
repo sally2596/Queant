01f48c1aa788533f326e304609a6834b2bea1a9d
rotter9027@gmail.com <rotter9027@gmail.com>
2022-08-15 16:01:48 +0900

    front_fix : month cal function

--- front-end/queant/src/views/portfolio/PortfolioView.vue
+++ front-end/queant/src/views/portfolio/PortfolioView.vue
@@ -368,23 +368,17 @@ export default {
       let result = [];
 
       let start = start_date.split("-");
-      let end = end_date.split("-");
-      let startYear = parseInt(start[0]);
-      let endYear = parseInt(end[0]);
+      //let end = end_date.split("-");
+      let year = parseInt(start[0]);
+			let month = parseInt(start[1]);
       let dates = [];
 
-      for (var i = startYear; i <= endYear; i++) {
-        var endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
-        var startMon = i === startYear ? parseInt(start[1]) - 1 : 0;
-        for (
-          var j = startMon;
-          j <= endMonth;
-          j = j > 12 ? j % 12 || 11 : j + 1
-        ) {
-          var month = j + 1;
-          var displayMonth = month < 10 ? "0" + month : month;
-          dates.push([i, displayMonth].join("-"));
-        }
+      for (let i=0; i <= term; i++) {
+        if (month==13) {
+					year++;
+					month=1;
+				}
+				dates.push([year, month].join("-"));
       }
 
       //원금
