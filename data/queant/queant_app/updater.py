e50c7338b9256add536f56da06f3251937c0f5f2
DongHyeon <72799490+Kameokong@users.noreply.github.com>
2022-08-18 15:48:05 +0900

    data_release: update code to change schedule time

--- data/queant/queant_app/updater.py
+++ data/queant/queant_app/updater.py
@@ -8,17 +8,17 @@ from .save_to_db import save_db_by_myself
 
 def start():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
-    scheduler.add_job(save_db, trigger='cron', hour=12, minute=55)
+    scheduler.add_job(save_db, trigger='cron', hour=5, minute=1)
     scheduler.start()
     
     
 def start_bank():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
-    scheduler.add_job(save_bank_db, trigger='cron',hour=15, minute=46)
+    scheduler.add_job(save_bank_db, trigger='cron',hour=6, minute=1)
     scheduler.start()
     
     
 def start_post():
     scheduler = BackgroundScheduler(timezone='Asia/Seoul')
-    scheduler.add_job(save_postdata_db, trigger='cron',hour=12, minute=57)
+    scheduler.add_job(save_postdata_db, trigger='cron',hour=7, minute=1)
     scheduler.start()
